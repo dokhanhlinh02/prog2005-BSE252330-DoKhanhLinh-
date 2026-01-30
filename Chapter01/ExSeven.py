2d4d84e51b2c1617b3c1fe2807a276131151e2ab
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-01-30 16:54:42 +0700

    Create ExSeven.py

---
+++ Chapter01/ExSeven.py
@@ -0,0 +1,6 @@
+# Bài 7
+a = int(input("Nhập số nguyên thứ nhất:"))
+b = int(input("Nhập số nguyên thứ hai:"))
+c = int(input("Nhập số nguyên thứ ba:"))
+max_number = max(a,b,c)
+print("Số lớn nhất là:",max_number)
