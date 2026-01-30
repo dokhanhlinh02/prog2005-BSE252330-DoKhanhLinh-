1ecf2713135211594b62d6e7f9581cca1d933d0f
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-01-30 16:52:02 +0700

    Create ExFive.py

---
+++ Chapter01/ExFive.py
@@ -0,0 +1,11 @@
+# Bài 5
+name = "Linh"
+age = 18
+average_score = 9,5
+age_next_year = age + 1
+doubled_score = average_score * 2
+print("name",type(name))
+print("age",type(age))
+print("average_score",type(average_score))
+print("age next year",age_next_year)
+print("doubled_score",doubled_score)
