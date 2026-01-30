69fbefabfa198f577d1f41b6a442e34d9b12198e
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-01-30 16:51:05 +0700

    Create ExFour.py

---
+++ Chapter01/ExFour.py
@@ -0,0 +1,7 @@
+# Bài 4
+def sun_two_numbers(a,b):
+    return a+b
+number1 = int(input("Nhập số nguyên thứ nhất:"))
+number2 = int(input("Nhập số nguyên thứ hai:"))
+ketqua = sun_two_numbers(number1,number2)
+print("Tổng hai số nguyên:",ketqua)
