0b94562d737920c422b03000255f745ff2da2e4c
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-01-30 17:00:06 +0700

    Create ExThirteen.py

---
+++ Chapter01/ExThirteen.py
@@ -0,0 +1,8 @@
+# Bài 13
+a = int(input("Nhập số chia nguyên thứ nhất:"))
+b = int(input("nhập số chia nguyên thứ hai:"))
+if b == 0:
+    print("Lỗi : Không thể chia cho 0")
+else:
+    result = a / b
+    print("Kết quả phép chia là:",result)
