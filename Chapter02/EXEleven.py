eb7e1e65477257f68c765ccf4ee3651e60c46daf
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-02-05 10:37:07 +0700

    Create EXEleven.py

---
+++ Chapter02/EXEleven.py
@@ -0,0 +1,10 @@
+# Bài 11:
+dtb = float(input("Nhập điểm trung bình:"))
+if dtb >= 8:
+    print("Giỏi")
+elif 6.5 <= dtb <= 8:
+    print("Khá")
+elif 5.0 <= dtb <= 6.5:
+    print("Trung bình")
+else:
+    print("Yếu")
