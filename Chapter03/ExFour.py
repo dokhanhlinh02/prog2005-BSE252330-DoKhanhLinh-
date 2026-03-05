7fa53625ef24412dfa569d0f8cc67c322d525276
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-03-05 09:19:16 +0700

    Create ExFour.py

---
+++ Chapter03/ExFour.py
@@ -0,0 +1,8 @@
+# Bài 4:
+numbers = [ 4, 27, 10, 13, 6, 32]
+numbers.sort()
+print(f" Danh sách theo thứ tự tăng dần: {numbers}")
+numbers.reverse()
+print(f" Đảo ngược danh sách: {numbers}")
+count_6 = numbers.count(6)
+print(f" Số lần số 5 xuất hiện: {count_6}")
