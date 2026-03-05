076b9b05047e30d6171c4e70456ca426a5cd4b85
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-03-05 09:02:20 +0700

    Create ExTwo.py

---
+++ Chapter03/ExTwo.py
@@ -0,0 +1,6 @@
+# Bài 2:
+friends = [ "linh","Trà","Nhi" ]
+friends.append ("Dung")
+print(f"các bạn sau khi đã thêm:{friends}")
+friends.pop(1)
+print(f"các bạn sau khi đã xóa vị trí thứ hai:{friends}")
