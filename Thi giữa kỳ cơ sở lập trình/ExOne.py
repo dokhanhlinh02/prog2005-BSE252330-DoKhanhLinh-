ab176e316965f4898db3ee840c5edc19f326fdd3
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-03-09 17:33:50 +0700

    Create ExOne.py

---
+++ Thi giữa kỳ cơ sở lập trình/ExOne.py
@@ -0,0 +1,8 @@
+# Bài 1:
+a = input("Nhập hệ số a:")
+b = input("Nhập hệ số b:")
+c = input("Nhập hệ số c:")
+max_nub = max(a,b,c)
+min_nub = min(a,b,c)
+print("Số lớn nhất trong 3 số là:",max_nub)
+print("Số bé nhất trong 3 số là:",min_nub)
