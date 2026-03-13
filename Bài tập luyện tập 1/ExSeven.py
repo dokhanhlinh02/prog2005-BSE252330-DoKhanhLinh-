7939619bd8b2c9ae2d43ef7c36a8a5abb2b28370
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-03-13 17:47:09 +0700

    Create ExSeven.py

---
+++ Bài tập luyện tập 1/ExSeven.py
@@ -0,0 +1,9 @@
+# Bài 7:
+class Student:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+s1 = Student("Nhi", 8.0)
+s2 = Student("Trà", 8.2)
+print(s1.name, s1.score)
+print(s2.name, s2.score)
