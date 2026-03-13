4ba33a03d561d44fe398e99e4c4aa362c500b8c3
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-03-13 16:40:53 +0700

    Create ExTwo.py

---
+++ Bài tập luyện tập 1/ExTwo.py
@@ -0,0 +1,10 @@
+# Bài 2:
+import math
+a = float(input("Nhập số thứ nhất:"))
+b = float(input("Nhập số thứ hai:"))
+print(f"{a}^{b} = {a**b}")
+print(f"Căn bậc 2 của {a} là: = {math.sqrt(a)}")
+print(f"Chia lấy phần nguyên:{a} : {b} = {a//b}")
+print(f"Chia lấy phần dư: {a} : {b} = {a%b}")
+print(f"Làm tròn số {a} = {round(a)}")
+print(f"làm tròn số {b} = {round(b)}")
