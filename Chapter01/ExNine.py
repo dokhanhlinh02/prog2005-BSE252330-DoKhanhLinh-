b2b33faa5232ea6bee955324b3a06dc0d19a549e
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-03-21 11:59:07 +0700

    Update ExNine.py

--- Chapter01/ExNine.py
+++ Chapter01/ExNine.py
@@ -1,4 +1,4 @@
-# Bài 9
+# Bài 9: Yêu cầu: Kiểm tra đầu vào tuổi. Yêu cầu người dùng nhập tuổi. Kiểm tra xem tuổi có nằm trong khoảng 1 đến 120 không. In ra thông báo phù hợp
 age = int(input("Nhập tuổi:"))
 if age >= 1 and age <= 120:
     print("Tuổi hợp lệ")
