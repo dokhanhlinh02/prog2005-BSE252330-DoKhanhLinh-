a564855e6f94838d8d78af67dc1d1afab7a00179
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-03-21 11:56:07 +0700

    Update ExFour.py

--- Chapter01/ExFour.py
+++ Chapter01/ExFour.py
@@ -1,4 +1,4 @@
-# Bài 4
+# Bài 4: Yêu cầu: Hàm tính tổng hai số. Viết một hàm sum_two_numbers(a, b) trả về tổng của hai số nguyên. Gọi hàm và in ra kết quả
 def sun_two_numbers(a,b):
     return a+b
 number1 = int(input("Nhập số nguyên thứ nhất:"))
