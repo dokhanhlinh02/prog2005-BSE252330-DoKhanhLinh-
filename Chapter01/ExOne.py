d1c412981c8d66a1faba4b686427ab893e33eb62
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-03-21 11:54:12 +0700

    Update ExOne.py

--- Chapter01/ExOne.py
+++ Chapter01/ExOne.py
@@ -1,4 +1,4 @@
-# Bài 1
+# Bài 1: Yêu cầu: Tạo các biến với các kiểu dữ liệu khác nhau. Tạo và in ra ba biến: một số nguyên (integer), một số thực (float), và một chuỗi (string)
 integer_number = 10
 float_number = 9.637
 string_text = "Hello everyone!"
