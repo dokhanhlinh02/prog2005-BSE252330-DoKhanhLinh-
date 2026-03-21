338329b5d3b8130917b30f28a3edd4f94811ab30
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-03-21 11:58:43 +0700

    Update ExEight.py

--- Chapter01/ExEight.py
+++ Chapter01/ExEight.py
@@ -1,4 +1,4 @@
-# Bài 8
+# Bài 8: Yêu cầu: Hàm với đối số mặc định Viết một hàm greet(name="Student") in ra “Hello, Student!”. Gọi hàm có và không có đối số.
 def greet( name ="student"):
     print("Hello",name + "!")
 greet()
