304f19bf1b4f89b460727aa7d666dd0ed43e8a1b
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-03-21 12:00:01 +0700

    Update ExEleven.py

--- Chapter01/ExEleven.py
+++ Chapter01/ExEleven.py
@@ -1,4 +1,4 @@
-# Bài 11
+# Bài 11: Yêu cầu: Chuyển độ C sang độ F Viết một chương trình chuyển nhiệt độ từ độ C sang độ F. Yêu cầu người dùng nhập nhiệt độ theo độ C (kiểu float). Sử dụng công thức: F = C × 9/5 + 32. In kết quả ra dạng chuỗi định dạng
 C = float(input("Nhập nhiệt độ theo độ C:"))
 F = C * 9 / 5 + 32
 print("Nhiệt độ theo độ F là:",F)
