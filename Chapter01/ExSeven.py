f0491986bcc44f15dc0087c7268a831548a1dd6c
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-03-21 11:58:18 +0700

    Update ExSeven.py

--- Chapter01/ExSeven.py
+++ Chapter01/ExSeven.py
@@ -1,4 +1,4 @@
-# Bài 7
+# Bài 7: Yêu cầu: Tìm số lớn nhất trong ba số. Nhập ba số và in ra số lớn nhất
 a = int(input("Nhập số nguyên thứ nhất:"))
 b = int(input("Nhập số nguyên thứ hai:"))
 c = int(input("Nhập số nguyên thứ ba:"))
