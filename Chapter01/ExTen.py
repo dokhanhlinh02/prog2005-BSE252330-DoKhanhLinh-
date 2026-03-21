125e4cb7c75721c3460ea711121de69aba0bf4f4
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-03-21 11:59:36 +0700

    Update ExTen.py

--- Chapter01/ExTen.py
+++ Chapter01/ExTen.py
@@ -1,4 +1,4 @@
-# Bài 10
+# Bài 10: Yêu cầu: Đếm số lần xuất hiện của ký tự ‘a’. Nhập một chuỗi và đếm số lần chữ cái ‘a’ xuất hiện.
 s = input("Nhập một chuổi:")
 count = s.count("a")
 print("Số lần chữ 'a' xuất hiện là :",count)
