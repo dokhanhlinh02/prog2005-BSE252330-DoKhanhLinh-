e8ffaacc7a056fa59b2e04d06f9bf875d027e789
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-04-03 18:11:44 +0700

    Update ExFive.py

--- Luyện tập 14/ExFive.py
+++ Luyện tập 14/ExFive.py
@@ -1,11 +1,17 @@
 # Bài 5:
+class Book:
+    def __init__(self, name, price):
+        self._name = name
+        self._price = price
+    def get_name(self):
+        return self._name
+    def get_price(self):
+        return self._price
 books = [
-    ("Book 1", 300000),
-    ("Book 2", 500000),
-    ("Book 3", 100000)
+    Book("Book 1", 30000)
 ]
+tong = sum(book.get_price() for book in books)
 with open("books.txt", "w", encoding="utf-8") as f:
-    f.write("Book 1;30000\n")
-    f.write("Book 2;50000\n")
-    f.write("Book 3;100000\n")
-    f.write("Tong;180000\n")
+    for book in books:
+        f.write(f"{book.get_name()};{book.get_price()}\n")
+    f.write(f"Tong;{tong}\n")
