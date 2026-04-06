bfc10af8e3d792091a7fd6755eb7761ba58c4209
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-04-06 17:27:13 +0700

    Update ExFour.py

--- Luyện tập 15/ExFour.py
+++ Luyện tập 15/ExFour.py
@@ -1,7 +1,7 @@
 # Bài 4:
-a = int(input("Nhập điểm môn thứ nhất:"))
-b = int(input("Nhập điểm môn thứ hai:"))
-c = int(input("Nhập điểm môn thứ ba:"))
+a = float(input("Nhập điểm môn thứ nhất:"))
+b = float(input("Nhập điểm môn thứ hai:"))
+c = float(input("Nhập điểm môn thứ ba:"))
 TB = ( a + b + c) / 2
 if TB >= 8:
     print("Giỏi")
