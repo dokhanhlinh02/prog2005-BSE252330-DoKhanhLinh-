3bc4301a9629510875816cb91378872434a39e27
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-04-06 17:07:05 +0700

    Create ExOne.py

---
+++ Luyện tập 15/ExOne.py
@@ -0,0 +1,10 @@
+# Bài 1:
+a = int(input("nhập số nguyên thứ nhất:"))
+b = int(input("Nhập số nguyên thứ hai:"))
+print("Tổng:", a + b)
+print("Hiệu:", a - b)
+print("Tích:", a * b)
+if b != 0:
+    print("Thương:", a / b)
+else:
+    print("Không chia được")
